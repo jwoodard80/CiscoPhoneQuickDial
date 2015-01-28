e52a86360c76f56c4c761fe1e9c6cd7f791afdba
Jonathan Woodard <jonathan@jlwoodard.com>
2015-01-28 00:21:15 -0600

    Various fixes

--- QuickDial.py
+++ QuickDial.py
@@ -2,7 +2,12 @@ __author__ = 'jwoodard'
 
 # TODO -- Possibly display the last 5 numbers dialed and make them redialed with A-E options
 # TODO -- Encrypt FAC and Password
-# TODO -- DONE --First run prompt for creds and FAC, store creds to an encrypted file
+# TODO -- Find phone IP Automatically
+
+# TODO -- Rerun setup option??
+# TODO -- Check for any unauthorized response
+# TODO -- DONE -- Operating system check for ini_location
+# TODO -- DONE -- First run prompt for creds and FAC, store creds to an encrypted file
 # TODO -- DONE -- Take Input "ld918704036566" ld and change to 918704036566,FAC#
 # todo -- DONE -- if input longer than 4 digits, append 9
 # todo -- DONE -- if longer than 6 digts append 91
@@ -11,8 +16,20 @@ __author__ = 'jwoodard'
 
 import requests
 import getpass
+import platform
 from requests.auth import HTTPBasicAuth
 import ConfigParser
+
+
+def os_check():
+    locations = {
+        "Windows": "C:\\Users\\"+getpass.getuser()+"\\CiscoPhoneQuickDial\\QuickDialConfig.ini",
+        "Linux": "\\home\\USER\\.config\\QuickDialConfig\\QuickDialConfig.ini",
+        "Darwin": "Some OSX Location"
+        }
+    ini = locations[platform.system()]
+    return ini
+
 
 def setupconfig():
     setup = ConfigParser.SafeConfigParser()
@@ -23,12 +40,16 @@ def setupconfig():
     setup.set('Information', 'phone', raw_input("Enter your phone device name: "))
     setup.set('Information', 'ip', raw_input("Enter your phone IP address: "))
 
-    print"Writing config file...."
-
-    with open("C:\\Users\\jwoodard\\CiscoPhoneQuickDial\\QuickDialConfig.ini", 'w') as configfile:
-        setup.write(configfile)
-    print "Done"
-    return True
+    print"Attempting to write config file...."
+    try:
+        with open(ini_location, 'w') as configfile:
+            setup.write(configfile)
+            print "Done"
+            return True
+    except IOError:
+        print "ERROR: Unable to create file. Check write permissions to location"
+        print "Expected Location: " + ini_location
+        raise SystemExit()
 
 
 def configcheck(): # If config is not set, ask setup questions and create file
@@ -36,14 +57,13 @@ def configcheck(): # If config is not set, ask setup questions and create file
 
     while not test: # fails if no file, creates file and sets while test to true allowing
         try:
-            ini_file = open("C:\\Users\\jwoodard\\CiscoPhoneQuickDial\\QuickDialConfig.ini")
+            ini_file = open(ini_location)
             test = True
         except IOError:
             print "Error: Could not find config file... Running setup.\n\n"
             setupconfig()
 
-    parser = ConfigParser.ConfigParser()
-    parser.readfp(ini_file)
+    parser = ConfigParser.ConfigParser().readfp(ini_file)
     return parser
 
 
@@ -61,32 +81,27 @@ def oopsfac(x):
 
 
 def prompt():
-    print "Quick Favorites"
-    #for shortcut, call in parser.items('RecentCalls'):
-    #    print "({0}) = {1}".format(shortcut, call)
-
-    NumberToDial = raw_input("Enter (a-e) or number to dial: ")
+    NumberToDial = raw_input("Enter number to dial: ")
     return NumberToDial
 
 
 def formatdial(input_numbers): ##8704036566
-    
+
     fac = False
-    
+
     if str("f") in input_numbers:
         input_numbers = input_numbers.replace("f", '')
         fac = True
 
     if len(input_numbers) == 4:
         input_numbers = input_numbers
-        
+
     elif len(input_numbers) == 7:
         # noinspection PyAugmentAssignment
         input_numbers = '9' + input_numbers
     elif len(input_numbers) == 10:
-        ## FAC Check
 
-        if not fac: 
+        if not fac:
             input_numbers = oopsfac(input_numbers)
         else:
             input_numbers = addFAC(input_numbers)
@@ -101,17 +116,20 @@ def formatdial(input_numbers): ##8704036566
 
 
 def dialNumbers(number):
-    xml = 'XML=' + '<CiscoIPPhoneExecute><ExecuteItem Priority="0" URL="Dial:' + number + '"/></CiscoIPPhoneExecute>'
+    xml = '''
+            XML=<CiscoIPPhoneExecute>
+                    <ExecuteItem Priority="0" URL="Dial:''' + number + '''"/>
+                  </CiscoIPPhoneExecute>
+          '''
 
     headers = {
-        'Host': parser.get("Information", "ip"),  # TODO -- Find this IP Automatically
+        'Host': parser.get("Information", "ip"),
         'Connection': 'close',
         'Content-Type': 'application/x-www-form-urlencoded'
     }
-    print 'Calling: ' + number
-
+    print 'Attemping to Call: ' + number+'....'
     try:
-        r = requests.post('http://'+parser.get("Information", "ip")+'/CGI/Execute',
+        requests.post('http://'+parser.get("Information", "ip")+'/CGI/Execute',
                           data=xml,
                           headers=headers,
                           auth=HTTPBasicAuth(parser.get("Information", "User"), parser.get("Information", "password")))
@@ -119,10 +137,12 @@ def dialNumbers(number):
     except requests.ConnectionError, e:
         print e
 
+
+ini_location = os_check()
 parser = configcheck()
 dial = False
 
 while dial is False:
     dial = formatdial(prompt())
-    print dial
-    dialNumbers(dial)
+    print "Dialing "+dial+"....."
+    # dialNumbers(dial)
